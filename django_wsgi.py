bc69ff7fddcde2a55f77df0e1f697603576f9a3b
dabenlu803 <lesley14@126.com>
2016-03-24 14:58:28 +0800

    add wsgi entry file

--- django_wsgi.py
+++ django_wsgi.py
@@ -4,12 +4,10 @@
 import os
 import sys
 
-
 reload(sys)
 sys.setdefaultencoding('utf8')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SimpleDjangoSite.settings")
 
-from django.core.handlers.wsgi import WSGIHandler
-application = WSGIHandler()
-
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
